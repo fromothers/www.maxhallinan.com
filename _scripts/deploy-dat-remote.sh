b9b2e823bcd38f32d97bfc447dd784143d063fe5
Max Hallinan <maxhallinan@riseup.net>
2018-02-07 21:15:54 +0100

    Update remote dat deploy script

--- _scripts/deploy-dat-remote.sh
+++ _scripts/deploy-dat-remote.sh
@@ -2,5 +2,5 @@
 
 source .env
 
-ssh $DAT_REMOTE_USER@$DAT_REMOTE_HOST \
-  "cd $DAT_REMOTE_PATH; _scripts/deploy-dat.sh"
+ssh $REMOTE_DAT_USER@$REMOTE_DAT_HOST -t \
+  "source ~/.zshrc; cd $REMOTE_DAT_PATH; _scripts/deploy-dat.sh"
