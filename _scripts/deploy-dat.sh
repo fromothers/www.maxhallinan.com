65987095ea124e2543633fd890f798b4211439de
Max Hallinan <maxhallinan@riseup.net>
2018-02-21 10:13:46 +0000

    Update _scripts/deploy-dat.sh

--- _scripts/deploy-dat.sh
+++ _scripts/deploy-dat.sh
@@ -12,8 +12,5 @@ bundle exec jekyll build \
   --config "_config.yml,_config.dat.yml" \
   --destination $BUILD_PATH
 
-# Link to keys
-ln -s $DAT_PATH $BUILD_PATH/
-
 # Publish changes
-dat share --dir $BUILD_PATH
+dat share 
